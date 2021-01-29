0d989a7822b3c8d9360b4d05e7e47f0f7a584490
Tucker <leachtucker@gmail.com>
2021-01-29 14:57:18 -0800

    Deleted some defunct code

--- api/classes/router.js
+++ api/classes/router.js
@@ -4,8 +4,6 @@ const Classes = require('./classes-model');
 // GET a list of all posts. IF query strings are provided, search by those values. If not, return a list of all objects.
 // In a successful response, this endpoint always resolves to an _array_
 router.get('/', (req, res) => {
-    const { name, type, start_time, duration, intensity_level, location, attendees_amt, max_class_size, cancelled } = req.query;
-
     Classes.findBy({ ...req.query })
         .then(result => {
             res.status(200).json(result);
