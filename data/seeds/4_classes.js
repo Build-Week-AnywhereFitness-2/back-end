cb1bed4dacf866eb4e873740c6eef9f09ed923bf
Tucker <leachtucker@gmail.com>
2021-01-28 17:51:22 -0800

    Building out router for classes /api/classes

--- data/seeds/4_classes.js
+++ data/seeds/4_classes.js
@@ -5,8 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('classes').insert([
-        { name: "Yoga 101", start_time: "2021-01-28T00:14:56Z", duration: "2 hours", intensity_level: 1, location: "Reno, NV"},
-        { name: "Crossfit 102", start_time: "2021-01-28T00:14:56Z", duration: "2 hours", intensity_level: 3, location: "Colorado, NV"},
+        { name: "Yoga 101", type: 2, start_time: "2021-01-28T00:14:56Z", duration: "2 hours", intensity_level: 1, location: "Reno, NV"},
+        { name: "Crossfit 102", type: 3, start_time: "2021-01-28T00:14:56Z", duration: "2 hours", intensity_level: 3, location: "Carson City, NV"},
+        { name: "Boxing with Mike Tyson", type: 1, start_time: "2021-01-28T00:14:56Z", duration: "1.3 hours", intensity_level: 4, location: "Lake Tahoe, NV"},
       ]);
     });
 };
