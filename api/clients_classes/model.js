beaff0e2da280db323e6c8b06bfb8ce8eef576fd
Tucker <leachtucker@gmail.com>
2021-02-02 13:24:33 -0800

    Narrowed the selection of some queries

--- api/clients_classes/model.js
+++ api/clients_classes/model.js
@@ -7,7 +7,8 @@ function find() {
 function findByUserId(user_id) {
     return db('clients_classes as cC')
         .where({ user_id })
-        .join('classes as c', 'c.id', '=', 'cC.class_id');
+        .join('classes as c', 'c.id', '=', 'cC.class_id')
+        .select('id', 'name as class_name', 'type as class_type', 'start_time', 'duration_hour', 'intensity_level', 'location', 'attendees_amt', 'max_class_size', 'cancelled');
 }
 
 async function add(user_id, class_id) {
--- api/instructors_classes/model.js
+++ api/instructors_classes/model.js
@@ -7,7 +7,8 @@ async function find() {
 function findByUserId(user_id) {
     return db('instructors_classes as iC')
         .where({ user_id })
-        .join('classes as c', 'c.id', '=', 'iC.class_id');
+        .join('classes as c', 'c.id', '=', 'iC.class_id')
+        .select('id', 'name as class_name', 'type as class_type', 'start_time', 'duration_hour', 'intensity_level', 'location', 'attendees_amt', 'max_class_size', 'cancelled');
 }
 
 async function add(user_id, class_id) {
