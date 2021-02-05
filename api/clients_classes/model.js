01cc85554c0cadb61157a5b70fe86d1e1eff211c
Tucker <leachtucker@gmail.com>
2021-02-04 17:21:02 -0800

    fixing some reponses

--- api/clients_classes/model.js
+++ api/clients_classes/model.js
@@ -8,7 +8,7 @@ function findByUserId(user_id) {
     return db('clients_classes as cC')
         .where({ user_id })
         .join('classes as c', 'c.id', '=', 'cC.class_id')
-        .select('id', 'name as class_name', 'type as class_type', 'start_time', 'duration_hour', 'intensity_level', 'location', 'attendees_amt', 'max_class_size', 'cancelled');
+        .select('id', 'c.name as name', 'type as class_type', 'start_time', 'duration_hour', 'intensity_level', 'location', 'attendees_amt', 'max_class_size', 'cancelled');
 }
 
 async function add(user_id, class_id) {
